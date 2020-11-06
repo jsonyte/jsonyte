96d75e919fb213cc23a0c102e737a40f020feb88
Robert Coltheart <robert.coltheart@gmail.com>
2020-11-06 22:18:24 +0800

    json api version

---
+++ src/JsonApi/Converters/JsonApiVersionConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonApi.Converters
+{
+    internal class JsonApiVersionConverter : JsonConverter<Version>
+    {
+        private static readonly Version MinimumVersion = Version.Parse("1.0");
+
+        public override Version Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+
+            if (string.IsNullOrEmpty(value) || !Version.TryParse(value, out var version))
+            {
+                throw new JsonApiException($"Invalid JSON:API version: '{value}'");
+            }
+
+            if (version < MinimumVersion)
+            {
+                throw new JsonApiException("JSON:API version is before than '1.0', minimum required version is '1.0'");
+            }
+
+            return version;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Version value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/JsonApi/JsonApiObject.cs
+++ src/JsonApi/JsonApiObject.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using JsonApi.Converters;
 
 namespace JsonApi
 {
     public class JsonApiObject
     {
         [JsonPropertyName("version")]
+        [JsonConverter(typeof(JsonApiVersionConverter))]
         public Version Version { get; set; }
 
         [JsonPropertyName("meta")]
