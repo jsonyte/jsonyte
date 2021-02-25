c152195964a4a15036634e418b24f1b91ef2c08a
Robert Coltheart <robert.coltheart@gmail.com>
2021-02-25 20:44:10 +0800

    Initial commit

--- src/JsonApi.Tests/ValidationTests.cs
+++ src/JsonApi.Tests/ValidationTests.cs
@@ -1,7 +1,14 @@
-﻿namespace JsonApi.Tests
+﻿using System;
+using System.Text.Json;
+using Xunit;
+
+namespace JsonApi.Tests
 {
     public class ValidationTests
     {
-        
+        [Fact]
+        public void Test()
+        {
+        }
     }
 }
---
+++ src/JsonApi/Helpers/Prototype.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json;
+
+namespace JsonApi.Helpers
+{
+    public static class Prototype
+    {
+        public static JsonDocument JsonDocumentFromObject<TValue>(TValue value, JsonSerializerOptions options = default)
+        {
+            return JsonDocumentFromObject(value, typeof(TValue), options);
+        }
+
+        public static JsonDocument JsonDocumentFromObject(object value, Type type, JsonSerializerOptions options = default)
+        {
+            var bytes = JsonSerializer.SerializeToUtf8Bytes(value, options);
+
+            return JsonDocument.Parse(bytes);
+        }
+
+        public static JsonElement JsonElementFromObject<TValue>(TValue value, JsonSerializerOptions options = default)
+        {
+            return JsonElementFromObject(value, typeof(TValue), options);
+        }
+
+        public static JsonElement JsonElementFromObject(object value, Type type, JsonSerializerOptions options = default)
+        {
+            using var doc = JsonDocumentFromObject(value, type, options);
+
+            return doc.RootElement.Clone();
+        }
+    }
+}
