347264eedf023372b3e2a7f2aecae16a0db28f45
Robert Coltheart <robert.coltheart@gmail.com>
2021-03-31 15:10:33 +0800

    custom converter tests

--- src/JsonApi.Tests/Deserialization/CustomDeserializationTests.cs
+++ src/JsonApi.Tests/Deserialization/CustomDeserializationTests.cs
@@ -1,7 +1,31 @@
-﻿namespace JsonApi.Tests.Deserialization
+﻿using JsonApi.Tests.Models;
+using Xunit;
+
+namespace JsonApi.Tests.Deserialization
 {
     public class CustomDeserializationTests
     {
-        
+        [Fact]
+        public void CanDeserializeWithCustomConverterAttribute()
+        {
+            const string json = @"
+                {
+                  'data': {
+                    'type': 'articles',
+                    'id': '1',
+                    'attributes': {
+                      'title': 'book',
+                      'isbn': '1231-4561-4587-0'
+                    }
+                  }
+                }";
+
+            var article = json.Deserialize<ArticleWithIsbnAndConverter>();
+
+            Assert.Equal("1231", article.Isbn.Part1);
+            Assert.Equal("4561", article.Isbn.Part2);
+            Assert.Equal("4587", article.Isbn.Part3);
+            Assert.Equal("0", article.Isbn.Part4);
+        }
     }
 }
--- src/JsonApi.Tests/Serialization/CustomSerializationTests.cs
+++ src/JsonApi.Tests/Serialization/CustomSerializationTests.cs
@@ -1,7 +1,34 @@
-﻿namespace JsonApi.Tests.Serialization
+﻿using JsonApi.Tests.Models;
+using Xunit;
+
+namespace JsonApi.Tests.Serialization
 {
     public class CustomSerializationTests
     {
-        
+        [Fact]
+        public void CanSerializeWithCustomConverterAttribute()
+        {
+            var article = new ArticleWithIsbnAndConverter
+            {
+                Id = "1",
+                Type = "articles",
+                Title = "book",
+                Isbn = new Isbn("1231", "4561", "4587", "0")
+            };
+
+            var json = article.Serialize();
+
+            Assert.Equal(@"
+                {
+                  'data': {
+                    'type': 'articles',
+                    'id': '1',
+                    'attributes': {
+                      'title': 'book',
+                      'isbn': '1231-4561-4587-0'
+                    }
+                  }
+                }".Format(), json, JsonStringEqualityComparer.Default);
+        }
     }
 }
