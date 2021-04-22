e1ca9f6bb159e391892eef4945c25b9e8820d604
Robert Coltheart <robert.coltheart@gmail.com>
2021-04-22 15:54:09 +0800

    tidy

--- src/Jsonyte/Serialization/MemberRef.cs
+++ src/Jsonyte/Serialization/MemberRef.cs
@@ -4,17 +4,17 @@ namespace Jsonyte.Serialization
 {
     internal readonly struct MemberRef
     {
+        public readonly ulong Key;
+
+        public readonly IJsonMemberInfo Member;
+
+        public readonly byte[] Name;
+
         public MemberRef(ulong key, IJsonMemberInfo member, byte[] name)
         {
             Key = key;
             Member = member;
             Name = name;
         }
-
-        public readonly ulong Key;
-
-        public readonly IJsonMemberInfo Member;
-
-        public readonly byte[] Name;
     }
 }
