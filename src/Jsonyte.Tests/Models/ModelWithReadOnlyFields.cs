47fb085d25140469ad44189966f5f2bf99cc4651
Robert Coltheart <robert.coltheart@gmail.com>
2021-04-29 19:40:08 +0800

    Add tests for ignore fields and properties

---
+++ src/Jsonyte.Tests/Models/ModelWithReadOnlyFields.cs
@@ -0,0 +1,21 @@
+﻿namespace Jsonyte.Tests.Models
+{
+    public class ModelWithReadOnlyFields
+    {
+        public string Id = "1";
+
+        public string Type = "model";
+
+        public readonly int? IntValue = 5;
+
+        public readonly string StringValue = "str";
+
+        public readonly Author Author = new()
+        {
+            Id = "4",
+            Type = "author",
+            Name = "bob",
+            Twitter = "bo"
+        };
+    }
+}
