fcc53e9c092f448c2f06b4acf7c9947eea986cee
Joel Loeshelle <jloeshelle@alarm.com>
2022-07-12 20:29:35 -0400

    Cleanup after code review for specifying the resource type via an Attribute

--- src/Jsonyte/Serialization/Metadata/JsonTypeInfo.cs
+++ src/Jsonyte/Serialization/Metadata/JsonTypeInfo.cs
@@ -1,12 +1,11 @@
-﻿using Jsonyte.Serialization.Attributes;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Jsonyte.Serialization.Attributes;
 
 namespace Jsonyte.Serialization.Metadata
 {
